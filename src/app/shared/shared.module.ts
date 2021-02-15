28989232820e41a460fcd6b20609f6bfd474a87f
Samuel-dot-cloud <successam336@gmail.com>
2021-02-15 10:57:05 +0300

    Adding components for the various blog sections

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../material.module';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ServicesComponent } from './services/services.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { NewsletterComponent } from './newsletter/newsletter.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent, AboutComponent, SkillsComponent, ServicesComponent, ProjectsComponent, NewsletterComponent, FooterComponent],
   imports: [
     CommonModule,
     MaterialModule
