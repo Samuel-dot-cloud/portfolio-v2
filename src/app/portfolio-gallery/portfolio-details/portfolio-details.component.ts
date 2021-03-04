c6381f9039061f8e126a432d5ada47f0900e005f
Samuel-dot-cloud <successam336@gmail.com>
2021-03-04 11:53:12 +0300

    Refining portfolio details section

--- src/app/portfolio-gallery/portfolio-details/portfolio-details.component.ts
+++ src/app/portfolio-gallery/portfolio-details/portfolio-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 import { ProjectImagesService } from '../../project-images.service';
 import { ActivatedRoute } from '@angular/router';
 
@@ -10,12 +10,22 @@ import { ActivatedRoute } from '@angular/router';
 export class PortfolioDetailsComponent implements OnInit {
   image: any;
 
-  constructor(private imageService: ProjectImagesService, private route: ActivatedRoute) { }
+  constructor(private imageService: ProjectImagesService, private route: ActivatedRoute, private el: ElementRef) { }
 
 
 
   ngOnInit() {
     this.image = this.imageService.getImage(this.route.snapshot.params['id']);
+
+    // we added this so that when the backdrop is clicked the modal is closed.
+    this.el.nativeElement.addEventListener('click', ()=> {
+      this.close()
+  })
+  }
+
+  close() {
+    this.el.nativeElement.classList.remove('sshow')
+    this.el.nativeElement.classList.add('hhidden')
   }
 
 }
