018512250a9da17a21caaedebdb9fc9a027c5c0c
Samuel-dot-cloud <successam336@gmail.com>
2021-03-10 17:49:21 +0300

    Adding and replacing images in project

--- src/app/project-images.service.ts
+++ src/app/project-images.service.ts
@@ -23,7 +23,7 @@ const Imagesdetails = [
     "name": "MyEdu",
     "description": "An MCQ Android application.",
     "category": "upcoming",
-    "url": "assets/img/portfolio/p1.jpg",
+    "url": "assets/img/portfolio/myedu.jpg",
     "github": "https://github.com/Samuel-dot-cloud/MyEdu",
     "live": ""
   },
@@ -32,7 +32,7 @@ const Imagesdetails = [
     "name": "Wallpaper App",
     "description": "An application that makes use of Unsplash's API to provide you with a catalog of photos, for use in theming your mobile phone.",
     "category": "latest",
-    "url": "assets/img/portfolio/p2.jpg",
+    "url": "assets/img/portfolio/wallpaper-app.jpg",
     "github": "https://github.com/Samuel-dot-cloud/WallpaperApp",
     "live": "https://appetize.io/app/kjgf04wqzeda66zvzxe5hjn9fr?device=nexus5&scale=75&orientation=portrait&osVersion=8.1"
   },
@@ -41,7 +41,7 @@ const Imagesdetails = [
     "name": "Kanban",
     "description": "A project that makes use of kanban methodology to keep track of tasks.",
     "category": "latest",
-    "url": "assets/img/portfolio/p3.jpg",
+    "url": "assets/img/portfolio/kanban.png",
     "github": "https://github.com/Samuel-dot-cloud/Kanban-Board",
     "live": ""
   },
@@ -50,7 +50,7 @@ const Imagesdetails = [
     "name": "Web-Speech-Analyzer",
     "description": "A website that makes use of various web APIs' to convert speech into text.",
     "category": "following",
-    "url": "assets/img/portfolio/p4.jpg",
+    "url": "assets/img/portfolio/speech-to-text.jpg",
     "github": "https://github.com/Samuel-dot-cloud/Speech-To-Text-Analyzer",
     "live": ""
   },
@@ -59,7 +59,7 @@ const Imagesdetails = [
     "name": "WhatsApp2",
     "description": "A clone of the popular WhatsApp application. The application maintains the base minimum functionalities of WhatsApp such as a group chat, an individual chat section,and the ability to send, receive and delete messages and documents such as PDF, image and MS word files.",
     "category": "popular",
-    "url": "assets/img/portfolio/p5.jpg",
+    "url": "assets/img/portfolio/whatsapp-2.jpg",
     "github": "https://github.com/Samuel-dot-cloud/WhatsApp2",
     "live": "https://appetize.io/app/uv9p412bztdu631cmwzx6fkzqg?device=nexus5&scale=75&orientation=portrait&osVersion=8.1"
   },
@@ -68,7 +68,7 @@ const Imagesdetails = [
     "name": "RecipeApp",
     "description": "An Android recipe search application.",
     "category": "popular",
-    "url": "assets/img/portfolio/p6.jpg",
+    "url": "assets/img/portfolio/recipe-app.jpg",
     "github":"https://github.com/Samuel-dot-cloud/RecipeApp",
     "live": ""
   }
